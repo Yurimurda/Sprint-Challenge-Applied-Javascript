af11693f06a9bbb0e06bf3daeef95d54390d174a
Yurimurda <lolsgotpwnd@yahoo.com>
2019-10-04 12:19:37 -0500

    Stuck on tabs, working on Cards

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,38 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topicsArray = [];
+
+
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then(response => {
+  console.log(response);
+  const qTab = document.querySelector('.topics');
+  response.array.forEach(element => {
+      return element;
+  });
+  const otherQTab = newTab(response.data);
+  qTab.appendChild(otherQTab);
+})
+.catch(error => {
+  console.log("The data was not returned", error);
+});
+
+
+
+
+
+
+
+function newTab (obj){
+    let tab = document.createElement('div');
+    
+    tab.classList.add('tab');
+    
+    tab.textContent = obj.tab;
+    
+    return tab;
+}
